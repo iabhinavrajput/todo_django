b7cd626ffa6dd79ef36379cc372555cbd9d52419
Abhinav <abhinav@Abhinavs-MacBook-Air.local>
2025-06-18 23:30:19 +0530

    Refactor task list view and add task creation endpoint

--- todo/api_urls.py
+++ todo/api_urls.py
@@ -3,7 +3,8 @@ from .import views
 
 urlpatterns = [
     path('', views.api_overview, name="api_overview"),
-    path('task_list/', views.task_list, name="task_list"),
+    path('task_list_get/', views.task_list_get, name="task_list_get"),
     path('task_detail/<int:id>/', views.task_detail, name="task_detail"),
+    path('task_create/', views.task_create, name="task_create"),
 
 ]
--- todo/views.py
+++ todo/views.py
@@ -64,7 +64,7 @@ def api_overview(request):
 
 
 @api_view(["GET"])
-def task_list(request):
+def task_list_get(request):
     tasks = Task.objects.all()
     serializer = TaskSerializer(tasks, many=True)
     return Response(serializer.data)
@@ -78,3 +78,11 @@ def task_detail(request,id):
         return Response({'error' : 'Task no found'}, status=404)
     serializer = TaskSerializer(task, many=False)
     return Response(serializer.data)
+
+@api_view(['POSt'])
+def task_create(request):
+    serializer = TaskSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
