f921b03373e371ed18266ebffb153ea837b4c189
iabhinavrajput <abhinavrajput207@gmail.com>
2025-06-19 15:32:02 +0530

    Add REST framework settings for authentication and permissions

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -30,6 +30,18 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
+
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
